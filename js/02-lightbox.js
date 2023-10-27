a454005fd74a868744e59d78f09ee448375e5792
YanaHlybovets <yana.hlybovets@gmail.com>
2023-10-27 18:24:28 +0200

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems.map(image =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML("beforeend", galleryHTML);
+
+
+let lightbox;
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') {
+        if (lightbox) {
+            lightbox.close();
+        }
+
+        lightbox = new SimpleLightbox('.gallery__link', {
+            captions: true,
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250,
+        });
+
+        lightbox.open();
+    }
+});
+
+
 
 console.log(galleryItems);
+
